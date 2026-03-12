e1a73b62e53e97223b2f0c19309f9e9ab59644e0
SoftDevCLF <cintya.lara.f@gmail.com>
2026-03-12 01:50:42 -0600

    Fix responsive design for logo and text

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,16 +1,16 @@
 import Image from "next/image";
 export default function Footer() {
   return (
-    <div className="w-full bg-[#6D2077] border-t border-[#6D2077] flex flex-row justify-between items-center py-3 xs:px-4 sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
-      <div className="text-[#F1FAF5] shrink">
+    <div className="w-full bg-[#6D2077] border-t border-[#6D2077] flex flex-row justify-between items-center gap-5 p-4 sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
+      <div className="text-[#F1FAF5] text-xs md:text-lg">
         <p>{new Date().getFullYear()}. Capstone Project for GBTAC, SAIT.</p>
       </div>
-      <div className="shrink object-scale-down">
+      <div className="relative w-[150px] sm:w-[170px] md:w-[200px] lg:w-[250px] h-[50px]">
         <Image
           src="/sait_extended_horizontal_reverse.png"
           alt="Logo"
-          width={200}
-          height={100}
+          fill
+          className="object-contain"
         />
       </div>
     </div>
