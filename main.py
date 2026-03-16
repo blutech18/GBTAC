e6be5c964f1c58ebac45c9692513542a36ff339c
isayabs <isabelleyabut@gmail.com>
2026-03-15 22:06:44 -0600

    Implemented and tested custom password reset email using Firebase Admin SDK and external mail services (SendGrid). Reverting to Firebase default reset email due to SAIT email filtering.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers.cistern import router as cistern_router
 
 # import routers here
 from routers.graphs import router as graph_router
+from routers.auth import router as auth_router
 
 app = FastAPI()
 app.add_middleware( CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"], )
 
 # add routers here
 app.include_router(graph_router)
-app.include_router(cistern_router)
+app.include_router(auth_router)
 
 @app.get("/")
 async def root():
