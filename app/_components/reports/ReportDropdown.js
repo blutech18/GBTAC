c920cb943f7189f6ef62490bdd9c3ffdafc1dbe3
SoftDevCLF <cintya.lara.f@gmail.com>
2026-03-17 23:43:26 -0600

    Change format by Prettier and delete dark

--- app/_components/reports/ReportDropdown.js
+++ app/_components/reports/ReportDropdown.js
@@ -2,9 +2,14 @@
 
 export default function ReportDropdown() {
   return (
-
     <div>
-      <p className="px-5 font-bold text-lg dark:text-black" style={{ fontFamily: "var(--font-titillium)" }}> Select The Report Type:</p>
+      <p
+        className="px-5 font-bold text-lg"
+        style={{ fontFamily: "var(--font-titillium)" }}
+      >
+        {" "}
+        Select The Report Type:
+      </p>
     </div>
-  )
+  );
 }
