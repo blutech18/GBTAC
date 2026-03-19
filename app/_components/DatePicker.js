273cd222c99930d64c6ff518e8dc7859a85d22ce
Temi Bankole <temibankole5@gmail.com>
2026-03-18 20:39:04 -0600

    UI fixing for date range

--- app/_components/DatePicker.js
+++ app/_components/DatePicker.js
@@ -28,7 +28,7 @@ export default function DateRangePicker({
 
   return (
     <div className="flex flex-wrap gap-4 items-end">
-      <div>
+      <div className="flex flex-col">
         <label className="block text-sm text-gray-600 mb-1">From</label>
         <input
           type="date"
@@ -46,7 +46,7 @@ export default function DateRangePicker({
         </div>
       </div>
 
-      <div>
+      <div className="flex flex-col">
         <label className="block text-sm text-gray-600 mb-1">To</label>
           <input
             type="date"
@@ -62,26 +62,27 @@ export default function DateRangePicker({
               <p className="text-red-500 text-xs mt-1">{errors.to}</p>
             )}
           </div>
-        <div>
       </div>
-      <div>
-        <label className="block text-sm text-gray-600 mb-1">Aggregation</label>
-          <div>
-            <TimeGranularityDropdown value={tempAggregation} onChange={setTempAggregation} />
-          </div>
-      </div>
-      
 
-      </div>
+      <div className="flex flex-col">
+        <label className="block text-sm text-gray-600 mb-1">Aggregation</label>
+        <TimeGranularityDropdown value={tempAggregation} onChange={setTempAggregation} />
+        <div className="h-4 mt-1"/>
+    </div>
+
+    <div className="flex flex-col">
+      <div className="h-5 mb-1" /> 
       <button
         onClick={setDates}
         disabled={hasErrors}
-        className={`px-4 py-2 mb-5 text-white font-semibold rounded transition self-end ${
+        className={`px-4 py-2 text-white font-semibold rounded transition ${
           hasErrors ? "bg-gray-400 cursor-not-allowed" : "bg-[#005EB8] hover:bg-[#004080]"
         }`}
       >
         Apply
       </button>
+      <div className="h-4 mt-1" /> 
     </div>
+  </div>
   );
 }
